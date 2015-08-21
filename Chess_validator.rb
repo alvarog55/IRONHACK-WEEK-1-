9f908b0f93860068cf32c10b96382c57fcad9cf0
alvarog55 <alvaroginebreda@gmail.com>
2015-08-21 12:52:45 +0200

    valid movements pawn

--- Chess_validator.rb
+++ Chess_validator.rb
@@ -11,9 +11,25 @@ class Printer
 	def print_board_positions(name)
 	puts name.chessboard
 	end
+	
+	def print_board_xy(name)
+		name.chessboard.map do |x|
+			x[0]= "0,1"
+			x[1] = "0,1"
+			x[2] = "0,1"
+			x[3] = "0,1"
+			x[4] = "0,1"
+			x[5] = "0,1"
+			x[6] = "0,1"
+			x[7] = "0,1"
+		end
+
+	end	
 end
 
 chess = Game.new
 hp = Printer.new
 
-puts hp.print_board_positions(chess)
+puts hp.print_board_xy(chess)
+puts chess.chessboard
+
