69595f24c50d0689905f71b1f3eafd8114cf4dc0
alvarog55 <alvaroginebreda@gmail.com>
2015-08-21 08:56:45 +0200

    iteration2

--- Shopping_cart.rb
+++ Shopping_cart.rb
@@ -7,11 +7,13 @@ class Product
 end
 
 class Shoppingcart
-    attr_reader :cart
+    attr_accessor :cart, :h, :cart_string, :cart_hash, :total
     def initialize
         @cart_string = [ ]
         @cart_hash = { }
         @total = 0
+        @h = Hash.new(0)
+        @manzanas = 0
     end
 
     def add_to_cart(product)
@@ -21,6 +23,12 @@ class Shoppingcart
     def print_cart_products
         string_to_hash
         puts @cart_hash
+    end
+
+    def print_cart_test
+        @cart_string.each do | item | 
+        @h.store(item.name, @h[item.name]+1) 
+        end
     end
 
     def print_total_price
@@ -41,6 +49,16 @@ class Shoppingcart
     end
 end
 
+
+class Contador_manzanas
+    @manzanas = 0
+    def leer_shoppingcart(shoppingcart,frutas)
+        @manzanas = shoppingcart.cart_string.count(frutas)
+        puts (@manzanas / 2.0).floor 
+    end
+
+end
+
 fruteria_Manoli = Shoppingcart.new
 
 apples = Product.new("apples",10)
@@ -57,4 +75,6 @@ fruteria_Manoli.add_to_cart(bananas)
 fruteria_Manoli.add_to_cart(watermelons)
 
 
-fruteria_Manoli.print_total_price
+contador = Contador_manzanas.new
+
+contador.leer_shoppingcart(fruteria_Manoli, apples)
